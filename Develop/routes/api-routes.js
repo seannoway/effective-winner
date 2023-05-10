eb09069c7000d16a601f0e5b67d42aeb5997d860
Sean Noway <sean.m.holian@gmail.com>
2023-05-10 12:51:19 -0400

    set routes for api-routes

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require ("fs");
+
+// defines GET request for endpoint api/notes
+router.get('/api/notes', async (req, res) => {
+  const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
+  res.json(dbJson);
+});
+
+// defines POSt request for endpoint api/notes
+router.post('/api/notes', (req, res) => {
+  const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+  const newFeedback = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+  dbJson.push(newFeedback);
+  fs.writeFileSync("db/db.json",JSON.stringify(dbJson));
+  res.json(dbJson);
+});
