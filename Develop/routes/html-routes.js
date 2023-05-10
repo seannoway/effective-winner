d7d1a7d969e5e8612ab9505ae0c6f5da82d5b543
Sean Noway <sean.m.holian@gmail.com>
2023-05-10 12:48:30 -0400

    added routes to html-routes

---
+++ Develop/routes/html-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const path = require('path');
+
+// defines route for index.html GET requests
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+// defines routes for notel.html GET requests
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+module.exports = router;
